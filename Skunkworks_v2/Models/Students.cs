a32e89c151092d3e11ad3808af69fa3c06e1d595
Jason Renaud <jason.renaud@peak.local>
2018-07-24 13:35:24 -0600

    Generated models from database, not sure they are needed

---
+++ Skunkworks_v2/Models/Students.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Skunkworks_v2.Models
+{
+    public partial class Students
+    {
+        public Students(string firstName, string lastName, int? age, int grade, string teacher)
+        {
+            
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Grade = grade;
+            Teacher = teacher;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int? Age { get; set; }
+        public int Grade { get; set; }
+        public string Teacher { get; set; }
+    }
+
+
+}
---
+++ Skunkworks_v2/Models/Teachers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Skunkworks_v2.Models
+{
+    public partial class Teachers
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Class { get; set; }
+        public string Student { get; set; }
+    }
+}
