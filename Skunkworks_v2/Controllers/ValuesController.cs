30a4614003f69d849688bf1b8aecbad80c1b78a6
Jason Renaud <jason.renaud@peak.local>
2018-07-27 12:32:13 -0600

    Implemented Delete{id} autoincrement posing problem however

--- Skunkworks_v2/Controllers/ValuesController.cs
+++ Skunkworks_v2/Controllers/ValuesController.cs
@@ -21,30 +21,36 @@ namespace Skunkworks_v2.Controllers
             string result = String.Format("It is currently the month of {0:MMMM}", time);
             return result;
         }
-    
-        
-        
-        
+
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public string Get(int id)
         {
-            return "value";
-        }
+            SqlCommand get_row = new SqlCommand("SELECT * FROM dbo.students WHERE _id = @id", conn);
+            get_row.Parameters.AddWithValue("@id", id);
+            string res = "";
+            conn.Open();
+            using (var reader = get_row.ExecuteReader())
+            {
+                reader.Read();
+                res = reader.GetString(0);
+                return res;
 
+            }
+            
+        }
         // POST api/values
         [HttpPost]
         public void Post()
         {
             conn.Open();    
-            string post_query = "INSERT INTO dbo.students(_id,FirstName, LastName, Age, Grade, Teacher) VALUES(@ID ,@FirstName, @LastName, @Age, @Grade,@Teacher)";
+            string post_query = "INSERT INTO dbo.students(FirstName, LastName, Age, Grade) VALUES(@FirstName, @LastName, @Age, @Grade)";
             SqlCommand exec = new SqlCommand(post_query, conn);
-            exec.Parameters.AddWithValue("@ID", 1);
-            exec.Parameters.AddWithValue("@FirstName", "James");
+            
+            exec.Parameters.AddWithValue("@FirstName", "Randy");
             exec.Parameters.AddWithValue("@LastName", "Dingle");
             exec.Parameters.AddWithValue("@Age", 15);
             exec.Parameters.AddWithValue("@Grade", 10);
-            exec.Parameters.AddWithValue("@Teacher", "Jessica Apple");
             exec.BeginExecuteNonQuery();
             conn.Close();
         }
@@ -59,6 +65,14 @@ namespace Skunkworks_v2.Controllers
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            int ID = id;
+            string del_query = "DELETE FROM dbo.students WHERE _id = @id";
+            conn.Open();
+            SqlCommand exec = new SqlCommand(del_query, conn);
+            exec.Parameters.AddWithValue("@id", ID);
+            exec.BeginExecuteNonQuery();
+            conn.Close();
+
         }
     }
 }
